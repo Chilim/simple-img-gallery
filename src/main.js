53e7dfb8fa04112f9c8014aadf4d989aba2202f1
Davud <davud@comp.fritz.box>
2018-11-06 01:15:36 +0300

    opacity bug fixed

--- src/main.js
+++ src/main.js
@@ -3,12 +3,8 @@ const main = document.querySelector('#main');
 const imgs = document.querySelectorAll('.imgs img');
 const opacity = 0.5;
 
-// const imgClick = evt => {
-//     main.src = evt.target.src;
-//     evt.target.style.opacity = opacity;
-// }
-
-function imgClick(evt) {
+const imgClick = (evt) => {
+  imgs.forEach(img => (img.style.opacity = 1));
   main.src = evt.target.src;
   evt.target.style.opacity = opacity;
 }
