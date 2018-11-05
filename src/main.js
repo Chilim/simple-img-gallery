f94be947e6965bd3daeab09fbb7d0ba0b20d9d70
Davud <davud@comp.fritz.box>
2018-11-06 01:26:29 +0300

    first image opacity

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ const main = document.querySelector('#main');
 const imgs = document.querySelectorAll('.imgs img');
 const opacity = 0.5;
 
+const [first, ...rest] = imgs;
+first.style.opacity = opacity;
+
 const imgClick = (evt) => {
   imgs.forEach(img => (img.style.opacity = 1));
   main.src = evt.target.src;
